b3ec9df30f572c4f18169d31e00eaa202a75f213
SwarnaNagaraju <swarnanagaraju77@gmail.com>
2026-03-31 22:34:02 +0530

    A5

---
+++ A4.java
@@ -0,0 +1,14 @@
+public class A4{
+    public static void main(String[] args){
+        int n[][] = new int[3][3];
+
+        for(int i=0; i<3 ;i++){
+            for(int j=0; j<3 ;j++){
+            
+                System.out.print(n[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ A5.java
@@ -0,0 +1,13 @@
+public class A5{
+    public static void main(String[] args){
+        int n[][] = new int[3][4];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                n[i][j] = (int)(Math.random()*10);
+                System.out.print(n[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
