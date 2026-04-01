5c65d4e1fc35613cbabc159c152c05cd08945101
SwarnaNagaraju <swarnanagaraju77@gmail.com>
2026-04-01 21:05:24 +0530

    A5

---
+++ A6.java
@@ -0,0 +1,22 @@
+public class A6{
+    public static void main(String[] args){
+        int n[][] = new int[4][];     // jagged
+        n[0] = new int[2];
+        n[1] = new int[3];
+        n[2] = new int[2];
+        n[3] = new int[5];
+
+        for(int i=0;i<n.length; i++){
+            for(int j=0; j<n[i].length;j++){
+                n[i][j] = (int)(Math.random()*100);
+            }
+        }
+
+        for(int nums[] : n){
+            for(int m : nums){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+    }
+}
